0006896fc10fe13f8d55d78b25d10fe9cf9ddcf3
RuslanTsymbal <russslan.ts@gmail.com>
2017-06-12 14:34:13 +0300

    ready-2v

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,10 +12,10 @@ $(document).ready(function() {
   //Validation email
 
    $('.form-button').click(function(){
-      const str = $('.form input[name=email]').val().trim();
-      const correct = str.length >= 6    // минимальная длина для точки, собаки, домена + по букве между ними
-      && str.includes('@')    // есть собака
-      && str.includes('.');   // есть точка
+      var str = $('.form input[name=email]').val().trim();
+      var correct = str.length >= 6
+      && str.includes('@')
+      && str.includes('.');
 
     if(!correct){
        $(".form").css({
